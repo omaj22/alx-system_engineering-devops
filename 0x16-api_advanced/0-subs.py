e8bd6939672b093d4ee7c882ec7bf76637754d82
omaj <ezedujichioma@yahoo.com>
2024-08-13 04:48:07 -0700

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,27 @@
-#!/usr/bin/python
-"""Function to query subscribers on a given Reddit subreddit."""
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-     except Exception:
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -9,3 +9,4 @@ How to use an API with pagination
 How to parse JSON results from an API
 How to make a recursive API call
 How to sort a dictionary by value
+~                                     
